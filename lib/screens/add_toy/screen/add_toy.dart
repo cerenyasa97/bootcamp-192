1ef57f296749a86e61e111eff1b41776f498fd2d
Ceren Yaşa <cerenyasa97@hotmail.com>
2022-06-06 23:53:35 +0300

    Add Toy screen codes were refactored.

--- lib/screens/add_toy/screen/add_toy.dart
+++ lib/screens/add_toy/screen/add_toy.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:swapy/core/base/view/base_view.dart';
 import 'package:swapy/core/constants/image/image_constants.dart';
 import 'package:swapy/core/extension/context_extension.dart';
 import 'package:swapy/core/extension/widget_extension.dart';
+import 'package:swapy/core/widget/button/custom_button.dart';
+import 'package:swapy/core/widget/text_field/text_field_with_label.dart';
 import 'package:swapy/screens/add_toy/view_model/add_toy_view_model.dart';
 
 class AddToy extends StatelessWidget {
@@ -10,20 +13,86 @@ class AddToy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BaseView<AddToyViewModel>(vmBuilder: AddToyViewModel(), builder: _buildPage);
+    return BaseView<AddToyViewModel>(
+        vmBuilder: AddToyViewModel(), builder: _buildPage);
   }
 
-  Widget _buildPage(BuildContext context, AddToyViewModel viewModel) => Scaffold(
-    body: SafeArea(
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 12),
-        children: [
-          Image.asset(
-            ImageConstants.addToy,
-            height: context.dynamicHeight(40),
-          ).widgetPadding(),
-        ],
+  Widget _buildPage(BuildContext context, AddToyViewModel viewModel) {
+    final textTheme = Theme.of(context).textTheme;
+    final space = SizedBox(
+      height: 10,
+    );
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 12),
+          children: [
+            Image.asset(
+              ImageConstants.addToy,
+              height: context.dynamicHeight(40),
+            ).widgetPadding(),
+            InkWell(
+              onTap: viewModel.selectImage,
+              child: Card(
+                child: viewModel.imageUrl != null
+                    ? CachedNetworkImage(imageUrl: viewModel.imageUrl!)
+                    : Container(
+                        width: context.dynamicHeight(250),
+                        height: context.dynamicHeight(250),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.add_a_photo_rounded,
+                              color: Colors.black,
+                              size: context.dynamicHeight(50),
+                            ),
+                            space,
+                            Text(
+                              'Fotoğraf Ekle',
+                              style: textTheme.headline6,
+                            )
+                          ],
+                        ),
+                      ),
+              ),
+            ),
+            space,
+            TextFieldWithLabel(
+              controller: viewModel.nameController,
+              label: 'Oyuncak Adı',
+              hintText: 'Lego...',
+            ),
+            space,
+            TextFieldWithLabel(
+              controller: TextEditingController(text: viewModel.gender ?? ''),
+              label: 'Cinsiyet',
+              itemSelected: (text) => viewModel.gender = text,
+              hintText: 'Seçiniz',
+              comboItems: ['Kız', 'Erkek', 'Unisex'],
+            ),
+            space,
+            TextFieldWithLabel(
+              controller: TextEditingController(
+                  text: viewModel.selectedToyType?.title ?? ''),
+              label: 'Kategori',
+              itemSelected: viewModel.updateToyType,
+              hintText: 'Seçiniz',
+              comboItems: viewModel.toyTypes.map((e) => e.title).toList(),
+            ),
+            space,
+            TextFieldWithLabel(
+              controller: viewModel.descController,
+              label: 'Açıklama',
+              hintText: '86 Parça Lego seti...',
+              maxLine: 10,
+            ),
+            space,
+            CustomButton(text: 'Kaydet', onTap: viewModel.saveToy)
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
--- lib/screens/add_toy/view_model/add_toy_view_model.dart
+++ lib/screens/add_toy/view_model/add_toy_view_model.dart
@@ -1,9 +1,165 @@
 import 'dart:async';
+import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:swapy/core/base/view_model/base_view_model.dart';
+import 'package:swapy/core/widget/dialog/util/show_confirm_dialog.dart';
+import 'package:swapy/screens/add_toy/service/add_toy_service.dart';
+import 'package:swapy/screens/home/model/toy_model.dart';
+import 'package:swapy/screens/home/model/toy_type_model.dart';
+import 'package:swapy/screens/home/service/home_service.dart';
+import 'package:swapy/screens/login/view_model/login_view_model.dart';
+import 'package:uuid/uuid.dart';
 
-class AddToyViewModel extends BaseViewModel{
+class AddToyViewModel extends BaseViewModel {
+  final service = AddToyService();
+  final homeService = HomeService();
+  final nameController = TextEditingController();
+  final descController = TextEditingController();
+  final picker = ImagePicker();
+  final storage = FirebaseStorage.instance.ref();
+  final user = LoginViewModel.instance.userInfo;
+  String? _gender;
+  TType? _selectedToyType;
+  List<TType> toyTypes = [];
+  String? imageUrl;
+
   @override
-  FutureOr<void> init() {
+  FutureOr<void> init() async {
+    toyTypes = (await homeService.getToyTypes()) ?? <TType>[];
+    if (toyTypes.length > 1) toyTypes.removeAt(0);
+  }
+
+  String? get gender => _gender;
+
+  set gender(String? value) {
+    _gender = value;
+    notifyListeners();
+  }
+
+  void updateToyType(String? type) {
+    selectedToyType = toyTypes.firstWhere((element) => element.title == type);
+  }
+
+  TType? get selectedToyType => _selectedToyType;
+
+  set selectedToyType(TType? value) {
+    _selectedToyType = value;
+    notifyListeners();
+  }
+
+  void selectImage() {
+    _showActionSheet(context);
+    notifyListeners();
+  }
+
+  void _showActionSheet(BuildContext context) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoActionSheet(
+        title: const Text('Fotoğraf Yükle'),
+        message: const Text(
+            'Eklemek istediğiniz oyuncağın fotoğrafını çekiniz veya fotoğraf seçiniz.'),
+        actions: <CupertinoActionSheetAction>[
+          CupertinoActionSheetAction(
+            isDefaultAction: true,
+            onPressed: () {
+              _getImage();
+              Navigator.pop(context);
+            },
+            child: const Text('Resim Çek'),
+          ),
+          CupertinoActionSheetAction(
+            onPressed: () {
+              _getImage(isFromGallery: true);
+              Navigator.pop(context);
+            },
+            child: const Text('Galeriden Seç'),
+          ),
+          CupertinoActionSheetAction(
+            isDestructiveAction: true,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('İptal'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future _getImage({isFromGallery = false}) async {
+    try {
+      await Permission.photos.request();
+      var permissionStatus = await Permission.photos.status;
+      if (permissionStatus.isGranted) {
+        //Select Image
+        XFile? pickedFile = await picker.pickImage(
+          source: isFromGallery ? ImageSource.gallery : ImageSource.camera,
+          maxWidth: 1000,
+          maxHeight: 1000,
+        );
+        if (pickedFile != null) {
+          File imageFile = File(pickedFile.path);
+          if (imageFile != null) {
+            //Upload to Firebase
+            var snapshot =
+                await storage.child(pickedFile.name).putFile(imageFile);
+            imageUrl = await snapshot.ref.getDownloadURL();
+            notifyListeners();
+          } else {
+            showConfirmDialog(
+                contentText: 'Fotoğraf yüklenirken bir sorun oluştu.',
+                isJustConfirm: true);
+          }
+        }
+      } else {
+        showConfirmDialog(
+            contentText: 'Fotoğraf yükleme izni verilmemiştir.',
+            isJustConfirm: true);
+      }
+    } on Exception catch (e) {
+      showConfirmDialog(contentText: e.toString(), isJustConfirm: true);
+    }
+  }
+
+  Future saveToy() async {
+    try {
+      if (nameController.text.isNotEmpty &&
+          selectedToyType != null &&
+          imageUrl != null) {
+        final toy = Toy(
+            toyId: Uuid().v1(),
+            name: nameController.text,
+            gender: gender,
+            advertiser: user?.name,
+            advertiserId: user?.userUid,
+            advertiserImage: user?.userImage,
+            advertiserRating: user?.rating,
+            image: imageUrl,
+            description: descController.text);
+        await service.saveToy(toy, selectedToyType?.typeId ?? "educational");
+        clearPage();
+      } else {
+        showConfirmDialog(
+            contentText:
+                'Lütfen oyuncak ismi, fotoğrafı ve kategorisini seçtiğinizden emin olunuz.',
+            isJustConfirm: true);
+      }
+    } on Exception catch (e) {
+      showConfirmDialog(contentText: e.toString(), isJustConfirm: true);
+    }
+  }
+
+  void clearPage(){
+    nameController.text = "";
+    descController.text = "";
+    gender = null;
+    selectedToyType = null;
+    imageUrl = null;
   }
 }
